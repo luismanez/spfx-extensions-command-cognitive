f62097ba958b2a376d391e0a8fa220a475f55ecb
luismanez <luis.manez@outlook.com>
2017-09-26 18:12:19 +0200

    Calling to Cognitive Services Vision API to get Tags of the selected Image

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Custom Command Set that gets metadata information from MS Cognitive Services for the selected Image
 
 ### Building the code
-?loadSPFX=true&debugManifestsFile=https://localhost:4321/temp/manifests.js&customActions={"2238922b-b040-418b-bc37-d32708ba821a":{"location":"ClientSideExtension.ListViewCommandSet.CommandBar"}}
+?loadSPFX=true&debugManifestsFile=https://localhost:4321/temp/manifests.js&customActions={"e109ab75-a728-418c-85c6-7430363e348d":{"location":"ClientSideExtension.ListViewCommandSet.CommandBar"}}
--- src/extensions/imageCognitiveMetadata/ImageCognitiveMetadataCommandSet.ts
+++ src/extensions/imageCognitiveMetadata/ImageCognitiveMetadataCommandSet.ts
@@ -6,10 +6,10 @@ import {
   IListViewCommandSetListViewUpdatedParameters,
   IListViewCommandSetExecuteEventParameters
 } from '@microsoft/sp-listview-extensibility';
+import { Dialog } from '@microsoft/sp-dialog';
+import { SPHttpClient, ISPHttpClientOptions, SPHttpClientResponse, IHttpClientOptions, HttpClientResponse, HttpClient } from '@microsoft/sp-http';
 
 import * as strings from 'ImageCognitiveMetadataCommandSetStrings';
-
-import { Dialog } from '@microsoft/sp-dialog';
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -17,40 +17,86 @@ import { Dialog } from '@microsoft/sp-dialog';
  * You can define an interface to describe it.
  */
 export interface IImageCognitiveMetadataCommandSetProperties {
-  // This is an example; replace with your own property
-  visible: boolean | undefined;
+  // This is an example; replace with your own properties
+  sampleTextOne: string;
+  sampleTextTwo: string;
 }
 
 const LOG_SOURCE: string = 'ImageCognitiveMetadataCommandSet';
 
-export default class ImageCognitiveMetadataCommandSet
-  extends BaseListViewCommandSet<IImageCognitiveMetadataCommandSetProperties> {
+export default class ImageCognitiveMetadataCommandSet extends BaseListViewCommandSet<IImageCognitiveMetadataCommandSetProperties> {
+
+  private cognitiveServicesKey: string = "[YOUR_KEY_HERE]";
+  private cognitiveServicesVisionUrl: string = `https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Adult,Categories,Color,Description,Faces,ImageType,Tags&subscription-key=${this.cognitiveServicesKey}`;
 
   @override
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, 'Initialized ImageCognitiveMetadataCommandSet');
-    return Promise.resolve<void>();
+    return Promise.resolve();
   }
 
   @override
   public onListViewUpdated(event: IListViewCommandSetListViewUpdatedParameters): void {
-    const command: Command | undefined = this.tryGetCommand('GET_IMAGE_METADATA');
-    if (command) {
-      Log.info(LOG_SOURCE, `Command ${command.id} loaded`);
-      if (event.selectedRows.length === 1) {
-        command.visible = true;        
-      } else {
-        command.visible = false;
-      }      
-      this.properties.visible = command.visible;
-    }    
+    const compareOneCommand: Command = this.tryGetCommand('COMMAND_1');
+    if (compareOneCommand) {
+      // This command should be hidden unless exactly one row is selected.
+      compareOneCommand.visible = event.selectedRows.length === 1;
+    }
   }
 
   @override
   public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
-    switch (event.commandId) {
-      case 'GET_IMAGE_METADATA':
-        alert(`Clicked ${strings.Command1}`);
+    switch (event.itemId) {
+      case 'COMMAND_1':        
+
+        Log.info(LOG_SOURCE, 'COMMAND_1');
+        console.log(event.selectedRows[0]);        
+
+        const kk: ISPHttpClientOptions = { };
+        const imageInfoUrl = event.selectedRows[0].getValueByName('.spItemUrl') + '&$select=@content.downloadUrl';
+        this.context.spHttpClient.fetch(imageInfoUrl, SPHttpClient.configurations.v1, kk).then((response: SPHttpClientResponse) => {
+            console.log(`Status text: ${response.statusText}`);
+            response.json().then((responseJSON: any) => {
+            console.log(responseJSON);
+            const imageDownloadUrl: string = responseJSON['@content.downloadUrl'];
+            console.log(imageDownloadUrl);
+
+            const requestHeaders: Headers = new Headers();
+            requestHeaders.append('Content-type', 'application/json');
+            requestHeaders.append('Cache-Control', 'no-cache');
+    
+            const body: string = JSON.stringify({
+              'Url': imageDownloadUrl
+            });
+            const httpOptions: IHttpClientOptions = {          
+              body: body,
+              headers: requestHeaders
+            }; 
+
+            this.context.httpClient.post(this.cognitiveServicesVisionUrl, 
+            HttpClient.configurations.v1, 
+            httpOptions).then((cognitiveResponse: HttpClientResponse) => {
+              // Access properties of the response object. 
+              console.log(`Status code: ${cognitiveResponse.status}`);
+              console.log(`Status text: ${cognitiveResponse.statusText}`);
+
+              //response.json() returns a promise so you get access to the json in the resolve callback.
+              cognitiveResponse.json().then((cognitiveResponseJSON: any) => {
+                let tagsInfo: string = '';
+                console.log(cognitiveResponseJSON);
+                const tags: any = cognitiveResponseJSON.tags;
+                tags.forEach(element => {
+                  tagsInfo = tagsInfo.concat(element.name, ", ");
+                });
+
+                Dialog.alert(tagsInfo);
+              }).catch((error: any) => {
+                console.log(error);
+                Dialog.alert(JSON.stringify(error));
+              });
+            });
+          });
+        });        
         break;
       default:
         throw new Error('Unknown command');
